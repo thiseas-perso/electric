862b719d3c47c15e94bb4de9ee6f9bc93d65a9d2
thiseaskambas <tkambas@yahoo.com>
2022-12-31 18:02:56 -0400

    cars data set as context

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,14 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { useState } from 'react';
+import { CarsCtx } from '../context/carsContext';
+
+const App = ({ Component, pageProps }) => {
+  const [carsState, setCarsState] = useState([]);
+
+  return (
+    <CarsCtx.Provider value={{ carsState, setCarsState }}>
+      <Component {...pageProps} />
+    </CarsCtx.Provider>
+  );
+};
+
+export default App;
--- pages/electric-cars/index.js
+++ pages/electric-cars/index.js
@@ -1,6 +1,27 @@
 import Head from 'next/head';
+import { useContext, useEffect } from 'react';
+import { CarsCtx } from '../../context/carsContext';
+import { getAllData } from '../../utils/csvParser';
 
-const Cars = () => {
+export async function getStaticProps() {
+  const allData = await getAllData();
+  return { props: { allData } };
+}
+
+const Cars = ({ allData }) => {
+  const { carsState, setCarsState } = useContext(CarsCtx);
+  useEffect(() => {
+    let isMounted = true;
+    if (carsState.length === 0 && isMounted) {
+      console.log('FETCHING DATA');
+      setCarsState(allData);
+    }
+    return () => {
+      isMounted = false;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log({ carsState });
   return (
     <>
       <Head>
