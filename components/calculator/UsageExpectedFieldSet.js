98f6f4284e89b6b9436088dd9ac07e406e1b099c
thiseaskambas <tkambas@yahoo.com>
2023-01-12 17:44:56 -0400

    checking for manadatory input fill, sending message on submit if input missing

--- components/calculator/UsageExpectedFieldSet.js
+++ components/calculator/UsageExpectedFieldSet.js
@@ -38,6 +38,7 @@ const UsageExpectedFieldSet = ({
           {errorState.usageExpected.totalKMPerY}
         </span>
         <input
+          autoFocus
           className="mb-3"
           min="0"
           placeholder="ex: 12500"
