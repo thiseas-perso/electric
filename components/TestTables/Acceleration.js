fdadafb56bb7e5d0637f256857caf2763ac7b68b
thiseaskambas <tkambas@yahoo.com>
2023-01-23 11:41:03 -0400

    acceleration is collapsing on mobile

--- components/TestTables/Acceleration.js
+++ components/TestTables/Acceleration.js
@@ -3,8 +3,7 @@ import React, { useState } from 'react';
 const Acceleration = ({ tests, className }) => {
   const [details, setDetails] = useState(false);
   const [moreDetails, setMoredetails] = useState(false);
-  const display = details ? '' : 'hidden';
-  const displayMore = moreDetails ? '' : 'hidden';
+
   return (
     <div className={className}>
       <table className=" min-w-full">
@@ -15,52 +14,312 @@ const Acceleration = ({ tests, className }) => {
         </caption>
         <thead>
           <tr>
-            <th>Motorisation</th>
-            <th className={displayMore}>0-10</th>
-            <th className={displayMore}>0-20</th>
-            <th className={displayMore}>0-30</th>
-            <th className={displayMore}>0-40</th>
-            <th className={displayMore}>0-50</th>
-            <th className={displayMore}>0-60</th>
-            <th className={displayMore}>0-70</th>
-            <th className={displayMore}>0-80</th>
-            <th className={displayMore}>0-90</th>
-            <th>0-100</th>
-            <th className={display}>0-100 (1ft)</th>
-            <th>0-100 (spec)</th>
-            <th className={display}>test VS spec</th>
-            <th className={display}>Date</th>
-            <th className={display}>Hp</th>
-            <th className={display}>Pneux</th>
-            <th>Poid (kg)</th>
-            <th className={display}>Roue avant</th>
-            <th className={display}>Roue arrière</th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              Motorisation
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-10
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-20
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-30
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-40
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-50
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-60
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-70
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-80
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-90
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-100
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-100 (1ft)
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              0-100 (spec)
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              test VS spec
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              Date
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              Hp
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              Pneux
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              Poid (kg)
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              Roue avant
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              Roue arrière
+            </th>
           </tr>
         </thead>
         <tbody>
           {tests.map((test, i) => {
             return (
               <tr key={i}>
-                <td>{test.Drive}</td>
-                <td className={displayMore}>{test.ZeroTo10}</td>
-                <td className={displayMore}>{test.ZeroTo20}</td>
-                <td className={displayMore}>{test.ZeroTo30}</td>
-                <td className={displayMore}>{test.ZeroTo40}</td>
-                <td className={displayMore}>{test.ZeroTo50}</td>
-                <td className={displayMore}>{test.ZeroTo60}</td>
-                <td className={displayMore}>{test.ZeroTo70}</td>
-                <td className={displayMore}>{test.ZeroTo80}</td>
-                <td className={displayMore}>{test.ZeroTo90}</td>
-                <td>{test.ZeroTo100}</td>
-                <td className={display}>{test.ZeroTo100onefoot}</td>
-                <td>{test.ZeroTo100spec}</td>
-                <td className={display}>{test.TestVsSpec}</td>
-                <td className={display}>{test.Date}</td>
-                <td className={display}>{test.Hp}</td>
-                <td className={display}>{test.Tires}</td>
-                <td>{test.Weight}</td>
-                <td className={display}>{test.WheelFront}</td>
-                <td className={display}>{test.WheelRear}</td>
+                <td
+                  data-th="Motorisation"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.Drive}
+                </td>
+                <td
+                  data-th="0-10"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo10}
+                </td>
+                <td
+                  data-th="0-20"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo20}
+                </td>
+                <td
+                  data-th="0-30"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo30}
+                </td>
+                <td
+                  data-th="0-40"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo40}
+                </td>
+                <td
+                  data-th="0-50"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo50}
+                </td>
+                <td
+                  data-th="0-60"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo60}
+                </td>
+                <td
+                  data-th="0-70"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo70}
+                </td>
+                <td
+                  data-th="0-80"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo80}
+                </td>
+                <td
+                  data-th="0-90"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo90}
+                </td>
+                <td
+                  data-th="0-100"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo100}
+                </td>
+                <td
+                  data-th="0-100 (1 foot)"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo100onefoot}
+                </td>
+                <td
+                  data-th="0-100 (spec)"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.ZeroTo100spec}
+                </td>
+                <td
+                  data-th="test VS spec"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.TestVsSpec}
+                </td>
+                <td
+                  data-th="Date"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.Date}
+                </td>
+                <td
+                  data-th="Hp"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.Hp}
+                </td>
+                <td
+                  data-th="Pneux"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.Tires}
+                </td>
+                <td
+                  data-th="Poid (kg)"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.Weight}
+                </td>
+                <td
+                  data-th="Roue avant"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.WheelFront}
+                </td>
+                <td
+                  data-th="Roue arrière"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.WheelRear}
+                </td>
               </tr>
             );
           })}
