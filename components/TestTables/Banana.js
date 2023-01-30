2794a9d8d136d3079278c4f2941e85139d9eabad
thiseaskambas <tkambas@yahoo.com>
2023-01-30 10:50:42 -0400

    banana table is responsive

--- components/TestTables/Banana.js
+++ components/TestTables/Banana.js
@@ -11,18 +11,57 @@ const Banana = ({ tests, className }) => {
         </caption>
         <thead>
           <tr>
-            <th>Coffre</th>
-            <th>Coffre sièges baissés</th>
-            <th>Coffre Avant</th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              Coffre
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              Coffre sièges baissés
+            </th>
+            <th
+              className={[
+                'absolute top-[-9999px] left-[-9999px] sm:static sm:top-0',
+              ]}
+            >
+              Coffre Avant
+            </th>
           </tr>
         </thead>
         <tbody>
           {tests.map((test, i) => {
             return (
               <tr key={i}>
-                <td>{test.Trunk}</td>
-                <td>{test.SeatsFolded}</td>
-                <td>{test.Frunk || 0}</td>
+                <td
+                  data-th="Trunk"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.Trunk}
+                </td>
+                <td
+                  data-th="SeatsFolded"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.SeatsFolded}
+                </td>
+                <td
+                  data-th="Frunk"
+                  className={[
+                    'block before:content-[attr(data-th)] before:font-semibold before:block sm:before:content-none sm:table-cell',
+                  ]}
+                >
+                  {test.Frunk || 0}
+                </td>
               </tr>
             );
           })}
