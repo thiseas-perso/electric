fe1aa2820a81f8f7296659e46b048749d4133b8e
thiseaskambas <tkambas@yahoo.com>
2023-02-14 14:35:26 -0400

    styled tests page

--- pages/tested-cars/index.js
+++ pages/tested-cars/index.js
@@ -2,32 +2,27 @@ import CarsHomeSection from '../../components/cars/CarsHomeSection';
 import MakerSection from '../../components/cars/MakerSection';
 import ModelsSection from '../../components/cars/ModelsSection';
 import CustomHead from '../../components/customHead';
+import Header from '../../components/header';
 
-import {
-  getAllData,
-  getAllDataLight,
-  getAllTestNames,
-} from '../../lib/csvParser';
+import { getAllDataLight, getAllTestNames } from '../../lib/csvParser';
 
 export async function getStaticProps() {
   const allDataLight = await getAllDataLight();
-  const allData = await getAllData();
+  // const allData = await getAllData();
   const testNames = getAllTestNames();
 
   return {
     props: {
       allDataLight,
       testNames,
-      allData,
     },
   };
 }
 
-const Cars = ({ allDataLight, testNames, allData }) => {
+const Cars = ({ allDataLight, testNames }) => {
   console.log({
     allDataLight,
     testNames,
-    allData,
   });
 
   const dataByMaker = allDataLight.reduce((acc, cur) => {
@@ -47,18 +42,17 @@ const Cars = ({ allDataLight, testNames, allData }) => {
   return (
     <>
       <CustomHead title="SOME TITLE" description="some description" />
-
-      <div className="min-h-[calc(100vh-48px)]">
-        <h1 className="text-xl p-2 text-white font-poppins font-extrabold text-center">
+      <Header className="flex items-center h-14 bg-light-primary-2 dark:bg-transparent" />
+      <div className="min-h-[calc(100vh-54px)]">
+        <h1
+          aria-label="Tous les tests"
+          className="text-xl p-2 text-white font-poppins font-extrabold text-center"
+        >
           Tous les tests
         </h1>
-        <MakerSection dataByMaker={dataByMaker} title="Cherchez par marque" />
-        <CarsHomeSection stringArr={testNames} title="Cherchez par test" />
-        <ModelsSection
-          allDataLight={allDataLight}
-          title="Tous les modèles testés"
-          last={true}
-        />
+        <MakerSection dataByMaker={dataByMaker} />
+        <CarsHomeSection stringArr={testNames} />
+        <ModelsSection allDataLight={allDataLight} last={true} />
       </div>
     </>
   );
