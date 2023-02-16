c72a08aae909bd580bc7369a5e9f97cbf93991c4
thiseaskambas <tkambas@yahoo.com>
2023-02-16 18:42:39 -0400

    added buy me a coffe widget

--- components/layout.js
+++ components/layout.js
@@ -4,6 +4,19 @@ import Header from './header';
 const Layout = ({ children, className }) => {
   return (
     <>
+      <script
+        data-name="BMC-Widget"
+        data-cfasync="false"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        data-id="eleco"
+        data-description="Support me on Buy me a coffee!"
+        data-message="En tant que projet personnel votre don est essentiel pour maintenir ce site en vie. Chaque petit geste est vraiment apprécié!"
+        data-color="#5F7FFF"
+        data-position="Right"
+        data-x_margin="18"
+        data-y_margin="18"
+        defer
+      />
       <main
         className={`${className} flex flex-grow flex-col min-h-full font-poppins`}
       >
