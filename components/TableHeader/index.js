078538cd984f08e1cd8a6d89a7daabc03067847f
thiseaskambas <tkambas@yahoo.com>
2023-02-17 16:31:33 -0400

    timeout for tableHeaders info

--- components/TableHeader/index.js
+++ components/TableHeader/index.js
@@ -1,15 +1,29 @@
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const TableHeader = ({ imageSrc, info }) => {
   const [clicked, setClicked] = useState(false);
+  const [timeoutId, setTimeoutId] = useState(null);
+
   const clickHandler = () => {
-    setClicked((prev) => !prev);
+    clearTimeout(timeoutId);
+    setClicked(() => true);
+    const newTimeoutId = setTimeout(() => {
+      setClicked(() => false);
+    }, 3000);
+    setTimeoutId(newTimeoutId);
   };
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [timeoutId]);
+
   return (
     <div>
       {clicked ? (
-        <div onClick={clickHandler}>{info}</div>
+        <div>{info}</div>
       ) : (
         <Image
           className="mx-auto unselectable"
