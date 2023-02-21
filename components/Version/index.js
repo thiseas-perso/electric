a64131dd03f6f5c4cff007d36e55fea100862382
thiseaskambas <tkambas@yahoo.com>
2023-02-21 08:34:43 -0400

    updated thousand csv

--- components/Version/index.js
+++ components/Version/index.js
@@ -1,8 +1,5 @@
-import Image from 'next/image';
 import React, { useState } from 'react';
 import VersionsTests from '../versionTests';
-import arrowButton from '../../public/icons/arrow-button.svg';
-import closeButton from '../../public/icons/close-button.svg';
 
 const Version = ({ version, testNames, filters }) => {
   const [display, setDisplay] = useState(false);
