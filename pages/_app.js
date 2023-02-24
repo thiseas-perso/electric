8f6a1b6b3f2a404664345afe3b2e919183657342
thiseaskambas <tkambas@yahoo.com>
2023-02-24 14:07:25 -0400

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
+import Script from 'next/script';
 import '../styles/globals.css';
 import { Raleway, Merriweather_Sans, Lato, Poppins } from '@next/font/google';
 import { ThemeProvider } from 'next-themes';
 import Layout from '../components/layout';
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 
 const raleway = Raleway({
   variable: '--raleway-font',
@@ -35,6 +38,8 @@ const merriweather = Merriweather_Sans({
 });
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+
   //gets screen size - to fix mobile viewport height problem
   useEffect(() => {
     // only execute all the code below in client side
@@ -44,7 +49,6 @@ const App = ({ Component, pageProps }) => {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
       }
-
       // Add event listener
       window.addEventListener('resize', handleResize);
 
@@ -56,8 +60,37 @@ const App = ({ Component, pageProps }) => {
     }
   }, []);
 
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider attribute="class">
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
+          
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <Layout
         className={`${raleway.variable} ${merriweather.variable} ${lato.variable} ${poppins.variable}`}
       >
